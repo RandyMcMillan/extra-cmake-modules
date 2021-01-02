5382b890225699e67995a2a1885484f0a3f8851d
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-01-02 19:21:37 +0100

    Improve error message and suggested commands

--- kde-modules/kde-git-commit-hooks/clang-format.sh.cmake
+++ kde-modules/kde-git-commit-hooks/clang-format.sh.cmake
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-# Copied from okular/hooks/pre-commit, all credits go to Albert Astals Cid
+# Based on okular/hooks/pre-commit, credits go to Albert Astals Cid
 
 readonly output=$(git clang-format -v --diff)
 
 if [[ "$output" == *"no modified files to format"* ]]; then exit 0; fi
 if [[ "$output" == *"clang-format did not modify any files"* ]]; then exit 0; fi
 
-echo "ERROR: you need to run git clang-format on your commit"
-echo "       git clang-format -f is potentially what you want"
+echo "ERROR: You have unformatted changes, please format your files. You can do this using the following commands:"
+echo "       git clang-format --force # format the changed parts"
+echo "       git clang-format --diff # preview the changes done by the formatter"
 exit 1
