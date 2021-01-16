7dc04d9c72a066d860eaca29fc32169d781e89e3
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-01-16 13:27:05 +0100

    Add clang-format target by default in KDEFrameworkCompilerSettings
    To prevent cmake errors with exiting usages it is checked if the target already exists.

--- kde-modules/KDEClangFormat.cmake
+++ kde-modules/KDEClangFormat.cmake
@@ -14,6 +14,8 @@
 #
 # Using this function will create a clang-format target that will format all
 # ``<files>`` passed to the function with the predefined KDE clang-format style.
+# Since version 5.79 this function is called by default in KDEFrameworkCompilerSettings. If directories should be excluded from
+# the formatting a .clang-format file with "DisableFormat: true" and "SortIncludes: false" should be created.
 #
 # Example usage:
 #
@@ -27,6 +29,7 @@
 
 #=============================================================================
 # SPDX-FileCopyrightText: 2019 Christoph Cullmann <cullmann@kde.org>
+# SPDX-FileCopyrightText: 2021 Alexander Lohnau <alexander.lohnau@gmx.de>
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -40,10 +43,14 @@ endif()
 
 # formatting target
 function(KDE_CLANG_FORMAT)
+    if (TARGET clang-format)
+        message(WARNING "the kde_clang_format function was already called")
+        return()
+    endif()
+
     # add target without specific commands first, we add the real calls file-per-file to avoid command line length issues
     add_custom_target(clang-format COMMENT "Formatting sources in ${CMAKE_CURRENT_SOURCE_DIR} with ${KDE_CLANG_FORMAT_EXECUTABLE}...")
-
-    # run clang-format only if available, else signal the user what is missing
+    # add target only if clang-format available
     if(KDE_CLANG_FORMAT_EXECUTABLE)
         foreach(_file ${ARGV})
             add_custom_command(TARGET clang-format
