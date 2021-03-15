14b5069dd83635a1865ad2f1acb5a141bb69c01a
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-03-15 17:55:52 +0100

    Output clang-format diff on small diffs

--- kde-modules/kde-git-commit-hooks/clang-format.sh
+++ kde-modules/kde-git-commit-hooks/clang-format.sh
@@ -10,4 +10,10 @@ if [[ "$output" == *"clang-format did not modify any files"* ]]; then exit 0; fi
 echo "ERROR: You have unformatted changes, please format your files. You can do this using the following commands:"
 echo "       git clang-format --force # format the changed parts"
 echo "       git clang-format --diff # preview the changes done by the formatter"
+
+# Output the diff to make it easy to review
+if [ ${#output} -lt 1000 ]
+then
+    git clang-format --diff
+fi
 exit 1
