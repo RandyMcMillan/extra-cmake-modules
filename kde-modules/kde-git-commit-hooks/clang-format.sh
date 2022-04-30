90563c67fb32cdc48cc4f08f53999b51e0ac887f
Alexander Lohnau <alexander.lohnau@gmx.de>
2022-04-30 08:57:26 +0200

    KDEGitCommitHooks: Check if KDEClangFormat was included

--- kde-modules/kde-git-commit-hooks/clang-format.sh
+++ kde-modules/kde-git-commit-hooks/clang-format.sh
@@ -5,8 +5,14 @@
 readonly output=$(git clang-format --extensions 'cpp,h,hpp,c' -v --diff)
 
 if [[ ! -f .clang-format ]]; then
-    echo "ERROR: no .clang-format file found in repository root, abort format"
-    echo "       run cmake for this repository to generate it"
+    if [[ @HAS_CLANG_FORMAT_COMMAND_INCLUDED@ = TRUE ]]; then
+        echo "ERROR: no .clang-format file found in repository root, abort format"
+        echo "       run cmake for this repository to generate it"
+    else
+        echo "ERROR: no .clang-format file found in repository root, abort format"
+        echo "Include the KDEClangFormat CMake module to copy the KDE .clang-format file during the CMake configuration"
+        echo "or manually copy a .clang-format file in the repository"
+    fi
     exit 1
 fi
 if [[ "$output" == *"no modified files to format"* ]]; then exit 0; fi
